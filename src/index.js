6ad330388a1fdc78035dc8146b09b2c794a7a679
Nitin Chandani <nitin1chandani@gmail.com>
2022-12-28 18:34:44 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css";
+import sdata from "./sdata";
+console.log(sdata);
+ReactDOM.render(
+  <>
+    <h1 className="heading">List of top 5 Web Series in 2022</h1>
+    <Card
+      imgsrc={sdata[0].imgsrc}
+      link={sdata[0].link}
+      sname={sdata[0].sname}
+      title={sdata[0].title}
+    />
+    <Card
+      imgsrc={sdata[1].imgsrc}
+      link={sdata[1].link}
+      sname={sdata[1].sname}
+      title={sdata[1].title}
+    />
+    <Card
+      imgsrc={sdata[2].imgsrc}
+      link={sdata[2].link}
+      sname={sdata[2].sname}
+      title={sdata[2].title}
+    />
+    <Card
+      imgsrc={sdata[3].imgsrc}
+      link={sdata[3].link}
+      sname={sdata[3].sname}
+      title={sdata[3].title}
+    />
+    <Card
+      imgsrc={sdata[4].imgsrc}
+      link={sdata[4].link}
+      sname={sdata[4].sname}
+      title={sdata[4].title}
+    />
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/sdata.js
@@ -0,0 +1,34 @@
+const sdata = [
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp7285322.jpg",
+    link: "https://www.primevideo.com/detail/0PDOKMV9CRLOMO5EUKNCUJLG4Q/ref=atv_sr_def_c_unkc_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07JJ77FD8&qid=1672214232130",
+    sname: "Mirzapur",
+    title: "Amazon Original Series",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/dwp1x/wp6075904.jpg",
+    link: "https://www.primevideo.com/detail/0S3QYI59BAEI5KVLHCKSR91YGD/ref=atv_sr_def_c_unkc_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07X443FLN&qid=1672215884511",
+    sname: "The Family Man",
+    title: "Amazon Original Series",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/dwp1x/wp4464499.jpg",
+    link: "https://www.primevideo.com/detail/0LL805LRLJUMK1H89CP0LFLBL5/ref=atv_sr_def_c_unkc_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B01MTOKPEF&qid=1672216018752",
+    sname: "Mr.Robot",
+    title: "Amazon Original Series",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp9448970.jpg",
+    link: "https://www.zee5.com/myaccount/subscription?utm_source=Search&utm_medium=Mark_TCPA&utm_campaign=Search_SVOD_Google_Hindi_Performance_Movie_Pitchers&utm_term=%7Bkeyword%7D&utm_content=Pitchers&gclid=CjwKCAiA76-dBhByEiwAA0_s9S8q0i6hgKRawHiikgYsE56qqcisaJPxfn9_Bg06-JWysrE8BZ57aBoC9C0QAvD_BwE&gclsrc=aw.ds",
+    sname: "Pitchers",
+    title: "Zee5 Original Series",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp7956080.jpg",
+    link: "https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta-story-1700000292",
+    sname: "Scam 1992",
+    title: "Amazon Original Series",
+  },
+];
+
+export default sdata;
